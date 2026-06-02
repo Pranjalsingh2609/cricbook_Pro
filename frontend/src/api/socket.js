a894a9184f8ee936754b89af1bae122472307e19
kanvamotu <pranjalsingh2609@gmail.com>
2026-06-03 04:54:07 +0530

    fix socket production url

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -1,2 +1,6 @@
-import { io } from 'socket.io-client';
-export const socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000', { autoConnect: true });
+import { io } from "socket.io-client";
+
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ["websocket"],
+  autoConnect: true,
+});
